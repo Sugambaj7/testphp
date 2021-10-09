ad17051ecd1c5a6c554ce7020be3255b6a7cb9d5
Sugambaj7 <81790901+Sugambaj7@users.noreply.github.com>
2021-10-09 21:08:34 +0545

    Add files via upload

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+echo "The form has been submitted";
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <a href="index.php">Click here to go to main page!!!</a>
+    </body>
+</html>
